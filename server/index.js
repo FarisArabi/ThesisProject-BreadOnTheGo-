89d3efe8542e19e059a39cb3e3ed3bca8d2c3b12
FarisArabi <faris.arabi89@gmail.com>
2018-06-02 14:21:45 +0300

    for heroku conflict

--- server/index.js
+++ server/index.js
@@ -74,39 +74,3 @@ app.listen(process.env.PORT || port , function () {
 //   })
 // }
 // module.exports = app
-/// //////////////////////////////////////////////////////////
-// const PORT = process.env.PORT || 3000
-//
-// if (!module.parent) {
-//   app.listen(PORT, () => {
-//     console.log(`The Port : ${PORT}`)
-//   })
-// }
-// module.exports = app
-/// //////////////////////////////////////////////////////////
-// const PORT = process.env.PORT || 3000
-//
-// if (!module.parent) {
-//   app.listen(PORT, () => {
-//     console.log(`The Port : ${PORT}`)
-//   })
-// }
-// module.exports = app
-/// //////////////////////////////////////////////////////////
-// const PORT = process.env.PORT || 3000
-//
-// if (!module.parent) {
-//   app.listen(PORT, () => {
-//     console.log(`The Port : ${PORT}`)
-//   })
-// }
-// module.exports = app
-/// //////////////////////////////////////////////////////////
-// const PORT = process.env.PORT || 3000
-//
-// if (!module.parent) {
-//   app.listen(PORT, () => {
-//     console.log(`The Port : ${PORT}`)
-//   })
-// }
-// module.exports = app
